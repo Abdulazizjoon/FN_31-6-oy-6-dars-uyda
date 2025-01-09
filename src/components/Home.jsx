59701c0f5a6127baec19d5d7d4db6c22a2373487
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2025-01-09 11:01:10 +0500

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Blogs from './blogs';
+import React from "react";
 import { Link } from "react-router-dom";
-import './index.css'
+import "./index.css";
 
 function Home() {
   return (
     <div>
-      <Link to="/Blogs" className="link">
+      <Link to="/blogs" className="link">
         Blogs
       </Link>
     </div>
   );
 }
 
-export default Home
+export default Home;
